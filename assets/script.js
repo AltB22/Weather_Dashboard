ebeb7c590f8b81de5e861776976f7d924e014a52
Alt B 2022 <massie.w@gmail.com>
2023-01-15 17:08:00 -0800

    Created directory structure and files required.  Created new API key from openweathermap.org and saved to JS file.

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+//Open Weather Base URL (with key) https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={eaf99af1f4ee974c35e4e4ec7368e660};
+
+// Weather_Dashboard API Key = {eaf99af1f4ee974c35e4e4ec7368e660};
+
