509cc5cff9f83e9768c0bcb622452da11876a617
Alt B 2022 <massie.w@gmail.com>
2023-03-04 20:41:07 -0800

    Adjusted API url to include template literal for searchedCIty and openWeatherApiKey'

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,16 @@
 // Weather_Dashboard API Key = {eaf99af1f4ee974c35e4e4ec7368e660};
 
 
-var OpenWeatherApiKey = "eaf99af1f4ee974c35e4e4ec7368e660";
+var openWeatherApiKey = "eaf99af1f4ee974c35e4e4ec7368e660";
 // var searchedCity = document.getElementById("")
-var SearchWeatherButton = document.getElementById("search-by-city-button")
+var searchWeatherButton = document.getElementById("search-by-city-button")
 
 function getWeather(event) {
     event.preventDefault();
     var searchedCity = document.getElementById("searched-city-input").value;
+    console.log(("searched-city-input").value)
 
-    var openWeatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={eaf99af1f4ee974c35e4e4ec7368e660}" + searchedCity;//concats the input value to the API
+    var openWeatherUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchedCity}&appid=${openWeatherApiKey}`;//concats the input value to the API
 
     document.getElementById("searched-city-input").innerHTML;
     fetch(openWeatherUrl)
