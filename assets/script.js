8e8f31d4b3b36aca541592f500e6b21bbc6b4be8
Alt B 2022 <massie.w@gmail.com>
2023-03-04 20:29:16 -0800

    Created vars for API Key, searchWeatherButton, searchedCity, and openWeatherUrl. Created function to fetch URL + searchedCity .then return response.json().  Still need to solve for geocoding of coordinates for proper functionality

--- assets/script.js
+++ assets/script.js
@@ -4,4 +4,22 @@
 
 
 var OpenWeatherApiKey = "eaf99af1f4ee974c35e4e4ec7368e660";
-var searchedCity = document.getElementById("")
+// var searchedCity = document.getElementById("")
+var SearchWeatherButton = document.getElementById("search-by-city-button")
+
+function getWeather(event) {
+    event.preventDefault();
+    var searchedCity = document.getElementById("searched-city-input").value;
+
+    var openWeatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={eaf99af1f4ee974c35e4e4ec7368e660}" + searchedCity;//concats the input value to the API
+
+    document.getElementById("searched-city-input").innerHTML;
+    fetch(openWeatherUrl)
+    .then(function (response) {
+        console.log(response.json())
+        return response.json();
+        
+    })
+}
+
+SearchWeatherButton.addEventListener('click',getWeather);//Event listener for Search Drink Button
