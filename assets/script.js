9b4d6db6fc35b5bb2c3accb80fb207868efd87ba
Alt B 2022 <massie.w@gmail.com>
2023-03-05 23:14:04 -0800

    Address console errors and got an array of objects returning in console by city per Geo Location feature of open weather API

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,29 @@
 //Open Weather Base URL (with key) https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={eaf99af1f4ee974c35e4e4ec7368e660};
 
 // Weather_Dashboard API Key = {eaf99af1f4ee974c35e4e4ec7368e660};
-var searchedCity = document.getElementById("searched-city-input").value;
-console.log(searchedCity);
+
+
 var openWeatherApiKey = "eaf99af1f4ee974c35e4e4ec7368e660";
-var openWeatherUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchedCity}&appid=${openWeatherApiKey}`;
+
 
 // var searchedCity = document.getElementById("")
 var searchWeatherButton = document.getElementById("search-by-city-button") 
 
-function getWeather(event, location) {
+function getWeather(event, searchedCity) {
     event.preventDefault();
-    
-    if(location) {
-        openWeatherUrl = ''
-    }
+    var searchedCity = document.getElementById("searched-city-input").value;
+    var openWeatherUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchedCity}&appid=${openWeatherApiKey}`;
+    console.log(searchedCity);
+    // if(location) {
+    //     openWeatherUrl = ''
+    // }
    
 
-    document.getElementById("searched-city-input").innerHTML;
+    document.getElementById("searched-city-input").innerHTML; 
     fetch(openWeatherUrl)
     .then(function (response) {
         console.log(response.json())
-        return response.json();
+        return response//.json();
         
     })
     // .then (function (cityData){
