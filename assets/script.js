3c46631bc64454a0431c757ca99353195e56143a
Alt B 2022 <massie.w@gmail.com>
2023-03-06 22:46:12 -0800

    Misc clean up

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,3 @@
-//Open Weather Base URL (with key) https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={eaf99af1f4ee974c35e4e4ec7368e660};
-
-// Weather_Dashboard API Key = {eaf99af1f4ee974c35e4e4ec7368e660};
 var searchHistoryEl = document.getElementById("history")
 var openWeatherApiKey = "eaf99af1f4ee974c35e4e4ec7368e660";
 var searchWeatherButton = document.getElementById("search-by-city-button") 
@@ -16,30 +13,36 @@ function getWeatherByCity(event, searchedCity) {
 
     console.log(searchedCity);
  
-    // document.getElementById("searched-city-input").innerHTML;
     fetch(openWeatherUrl)
     .then(function (response) {
-        // console.log(response.json())
+       
         return response.json();
     })
     .then(function (cityWeatherData) {
         console.log(cityWeatherData)
         console.log(cityWeatherData.main.temp)
-        for(let i = 0 ;i < cityWeatherData.main.length; i++) {
+        // for(let i = 0 ;i < cityWeatherData.main.length; i++) 
+        // {
             var cityContainerEl = document.createElement('input')
             cityContainerEl.textContent = cityWeatherData.name
+            console.log(cityWeatherData.name)
 
 
             
-        }
+        // }
     })
        
 };
 
+function renderCityHistory(){
+
+}
+
 function clearHistory(event) {
     event.preventDefault();
     localStorage.clear();
-    localStorageCityHistory
+    localStorageCityHistory = [];
+    renderCityHistory();
    
 }
 
