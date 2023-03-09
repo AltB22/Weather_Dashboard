cfe86266378b906147f06a41b78b8796d7e3462a
Alt B 2022 <massie.w@gmail.com>
2023-03-08 23:12:32 -0800

    worked on getLocalStorage() to grab unique key values from local storage and append as li's on a ul.  Still needs work

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var searchHistoryEl = document.getElementById("history")
+var searchHistoryEl = document.getElementById("city-history")
 var openWeatherApiKey = "eaf99af1f4ee974c35e4e4ec7368e660";
 var searchWeatherButton = document.getElementById("search-by-city-button") 
 var clearCityHistoryButton = document.getElementById("clear-history-btn") 
@@ -13,14 +13,14 @@ function getWeatherByCity(event, searchedCity) {
     if(!localStorage.getItem(searchedCity)) {
       localStorage.setItem(searchedCity, JSON.stringify(searchedCity));
       
-      if(localStorageCityHistory !== null) {
-        var cityHistoryContainterEl = document.getElementById("city-history")
-        cityHistoryContainterEl.setAttribute('id', 'city-btn')
-      }
+    //   if(localStorageCityHistory !== null) {
+    //     // searchHistoryEl.createElement('ul')
+    //     // searchHistoryEl.setAttribute('id', 'city-btn')
+    //     searchHistoryEl.innerHTML(searchedCity)
+
+    //   }
     }
 
-
-    
 
     var openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + openWeatherApiKey + "&units=imperial";
 
@@ -50,10 +50,17 @@ function getWeatherByCity(event, searchedCity) {
 function getLocalStorage(){
     var localStorageCities = {...localStorage };
     for(i = 0; i < localStorageCities.length; i++) {
+        // Object.keys(localStorage).forEach(key) => {
+            localStorageCities = createElement('ul');
+            localStorageCities.setAttribute('id', key)
+            var localStorageCity = JSON.parse(localStorage.getItem(key));
+            localStorageCity.textContent = localStorageCities
+            searchHistoryEl.append(localStorageCities);
+            localStorageCities.append(localStorageCity);
 
-    }
+        };
+    };
 
-};
 
 function clearHistory(event) {
     event.preventDefault();
