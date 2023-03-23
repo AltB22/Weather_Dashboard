f2fa40754eff86980aaa6db0c5c1043452c9a2cd
Alt B 2022 <massie.w@gmail.com>
2023-03-22 17:53:22 -0700

    Got getLocalStorage function working properly with buttons created and appended for each local storage key (city name).  May adjust this later to make unique key integer and use the corresponding value as the button text.

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,53 @@
-var searchHistoryEl = document.getElementById("city-history")
+// var searchHistoryEl = document.getElementById("city-history")
 var openWeatherApiKey = "eaf99af1f4ee974c35e4e4ec7368e660";
 var searchWeatherButton = document.getElementById("search-by-city-button")
 var clearCityHistoryButton = document.getElementById("clear-history-btn")
 let localStorageCityHistory = JSON.parse(localStorage.getItem("search")) || [];
 
+
 function getWeatherByCity(event, searchedCity) {
     event.preventDefault();
-
     var searchedCity = document.getElementById("searched-city-input").value;
-
     var openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + openWeatherApiKey + "&units=imperial";
 
     // let name = event.target.textContent; 
     if (!localStorage.getItem(searchedCity)) {
         localStorage.setItem(searchedCity, JSON.stringify(searchedCity));
-
-        //   if(localStorageCityHistory !== null) {
-        //     // searchHistoryEl.createElement('ul')
-        //     // searchHistoryEl.setAttribute('id', 'city-btn')
-        //     searchHistoryEl.innerHTML(searchedCity)
-        //   }
     }
 
     fetch(openWeatherUrl)
         .then(function (response) {
-
+            // console.log(response.json());
             return response.json();
+            
         })
         .then(function (cityWeatherData) {
             console.log(cityWeatherData)
-            console.log(cityWeatherData.main.temp)
+            console.log(cityWeatherData.main)
             for (let i = 0; i < cityWeatherData.main.length; i++) {
                 var cityContainerEl = document.createElement('input')
                 cityContainerEl.textContent = cityWeatherData.name
-                console.log(cityWeatherData.name)
-
-
-
+                // console.log(cityWeatherData.name)
             }
         })
 
 };
 
-function getLocalStorage() {
-    var localStorageCities = { ...localStorage };
-    for (i = 0; i < localStorageCities.length; i++) {
-        // Object.keys(localStorage).forEach(key) => {
-        localStorageCities = createElement('ul');
-        localStorageCities.setAttribute('id', key)
+let getLocalStorage = () => {
+    // var localStorageCities = { ...localStorage };
+    // for (i = 0; i < localStorageCities.length; i++) {
+        Object.keys(localStorage).forEach((key) => {
+        var searchHistoryEl = document.getElementById("city-history")
+        console.log(searchHistoryEl)
+        var cityButton = document.createElement('button');
+        cityButton.setAttribute('id', "city-btn");
         var localStorageCity = JSON.parse(localStorage.getItem(key));
-        localStorageCity.textContent = localStorageCities
-        searchHistoryEl.append(localStorageCities);
-        localStorageCities.append(localStorageCity);
+        cityButton.textContent = localStorageCity
+        // cityButton.innerText = localStorage(key)
+        searchHistoryEl.append(cityButton);
+       
 
-    };
+    });
 };
 
 
@@ -65,6 +59,7 @@ function clearHistory(event) {
 
 }
 
+getLocalStorage();
 
 searchWeatherButton.addEventListener('click', getWeatherByCity);//Event listener for city search Button
 clearCityHistoryButton.addEventListener('click', clearHistory);//Event listener for clear history Button
