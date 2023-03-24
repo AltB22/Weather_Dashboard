5424b91616bae0dba8992b1db467a921483af04e
Alt B 2022 <massie.w@gmail.com>
2023-03-24 08:58:39 -0700

    Working on script.js functions and corresponding event listeners/functions call.  Still not getting refresh of cityButton upon without refresh.  Still need to return proper object weather data and dynamically created cards.

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,81 @@
-// var searchHistoryEl = document.getElementById("city-history")
+// var searchHistoryEl = document.getElementById("city-history");
 var openWeatherApiKey = "eaf99af1f4ee974c35e4e4ec7368e660";
-var searchWeatherButton = document.getElementById("search-by-city-button")
-var clearCityHistoryButton = document.getElementById("clear-history-btn")
+var searchWeatherButton = document.getElementById("search-by-city-button");
+var clearCityHistoryButton = document.getElementById("clear-history-btn");
 let localStorageCityHistory = JSON.parse(localStorage.getItem("search")) || [];
-var searchHistoryEl = document.getElementById("city-history")
-var cityButton = document.createElement('button');
-var searchedCity = document.getElementById("searched-city-input").value;
-var openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + openWeatherApiKey + "&units=imperial";
-let weatherData = openWeatherUrl
+var searchHistoryEl = document.getElementById("city-history");
+var openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+let weatherData = openWeatherUrl;
 
-function getWeatherByCity(event, searchedCity) {
+
+
+function getWeatherByCity(event) {
     event.preventDefault();
-    
-    
-    // let name = event.target.textContent; 
+  
+    var searchedCity = document.getElementById("searched-city-input").value;
+    var openWeatherUrlCity = openWeatherUrl + searchedCity + "&appid=" + openWeatherApiKey + "&units=imperial";
+    // console.log(openWeatherUrlCity)
+   
     if (!localStorage.getItem(searchedCity)) {
         localStorage.setItem(searchedCity, JSON.stringify(searchedCity));
     }
 
-    fetch(openWeatherUrl)
+    fetch(openWeatherUrlCity)
         .then(function (response) {
             // console.log(response.json());
             return response.json();
-            
-        })
-        .then(function (cityWeatherData) {
-            console.log(cityWeatherData)
-            console.log(cityWeatherData.main)
-            // for (let i = 0; i < cityWeatherData.main.length; i++) {
-            //     var cityContainerEl = document.createElement('input')
-            //     cityContainerEl.textContent = cityWeatherData.name
-            //     // console.log(cityWeatherData.name)
-            // }
-        })
 
+        })
+        .then(function (weatherData) {
+
+         
+            if(searchWeatherButton) {
+              console.log(weatherData)
+            }
+
+        });
 };
+
 function renderTodayForecast(event) {
-event.preventDefault();
-var currentWeather = document.getElementById("current-weather");
-var todayWeatherCity = document.createElement('p');
-var currentWeatherData = document.createElement('ul');
-var currentWeatherLineItems = document.createElement('li')
-currentWeatherLineItems.textContent = openWeatherUrl;
-if(cityButton) {
-    todayWeatherCity = cityButton.textContent;
-}
-currentWeather.append(todayWeatherCity);
-currentWeather.append(currentWeatherData);
-currentWeather.append(currentWeatherLineItems)
+    // event.preventDefault();
+    Object.keys(localStorage).forEach((key) => {
+  
+    // var localStorageCity = JSON.parse(localStorage.getItem(key));
+    var cityButton = document.getElementById('city-btn');
+    var currentWeather = document.getElementById("current-weather");
+    var todayWeatherCity = document.createElement('h4');
+    var currentWeatherData = document.createElement('ul');
+    var currentWeatherLineItems = document.createElement('li')
+    currentWeatherLineItems.textContent = weatherData;
+    // console.log(weatherData)
+    if (key === cityButton.textContent) {
+        // console.log(cityButton)
+        todayWeatherCity.textContent = cityButton.textContent
+    }
+    currentWeather.append(todayWeatherCity);
+    currentWeather.append(currentWeatherData);
+    currentWeather.append(currentWeatherLineItems);
 
 
+    },
+   
+    )
+    
 }
 
 //Maybe adjust this later to make unique key integer and use the corresponding value as the button text?
 let getLocalStorage = () => {
-    // var localStorageCities = (localStorage, key);
-    // for (i = 0; i < localStorageCities.length; i++) {
-        Object.keys(localStorage).forEach((key) => {
-        
-        cityButton.setAttribute('id', "city-btn");
-        cityButton.addEventListener("click", renderTodayForecast);
-        var localStorageCity = JSON.parse(localStorage.getItem(key));
-        cityButton.textContent = localStorageCity;
-        searchHistoryEl.append(cityButton);
-       
 
+    Object.keys(localStorage).forEach((key) => {
+        var todayWeatherButton = document.createElement('button');
+        todayWeatherButton.setAttribute('id', "city-btn");
+        todayWeatherButton.addEventListener("click", renderTodayForecast);
+        searchWeatherButton.addEventListener('click', renderTodayForecast);
+        var localStorageCity = JSON.parse(localStorage.getItem(key));
+        todayWeatherButton.textContent = localStorageCity;
+        searchHistoryEl.append(todayWeatherButton);
+        renderTodayForecast();
+        
     }
     );
 };
@@ -74,12 +85,34 @@ function clearHistory(event) {
     event.preventDefault();
     localStorage.clear();
     localStorageCityHistory = [];
-    getLocalStorage();
+    // getLocalStorage();
+    
 
 }
 
 
-
-searchWeatherButton.addEventListener('click', getWeatherByCity);//Event listener for city search Button
-clearCityHistoryButton.addEventListener('click', clearHistory);//Event listener for clear history Button
 getLocalStorage();
+// getWeatherByCity();
+searchWeatherButton.addEventListener('click', getWeatherByCity);//Event listener for city search Button
+
+clearCityHistoryButton.addEventListener('click', clearHistory);//Event listener for clear history Button
+
+
+
+
+
+// const handleFormSubmit = async (event) => {
+//     event.preventDefault();
+
+//     // Since mutation function is async, wrap in a `try...catch` to catch any network errors from throwing due to a failed request.
+//     try {
+//       // Execute mutation and pass in defined parameter data as variables
+//       const { data } = await addProfile({
+//         variables: { name },
+//       });
+
+//       window.location.reload();
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
