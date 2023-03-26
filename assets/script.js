deefb0100c124e810376e6a365587f79df0ead6b
Alt B 2022 <massie.w@gmail.com>
2023-03-25 17:01:40 -0700

    Added fields to current weather for humidity, wind speed, and wind direction using dot notation per openWeather documentation

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,81 @@ var todaysForecast = document.getElementById("todays-forecast");
 // var openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="
 // let weatherData = openWeatherUrl;
 
+
+
+
+
+//Call to API passing the searched city name and concat it into the API along with the key and conversion to imperial system format.
+function getWeatherByCity(event) {
+    event.preventDefault();
+    var searchCity = document.getElementById("searched-city-input").value;
+
+    var openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + openWeatherApiKey + "&units=imperial";
+
+    currentWeather.innerHTML = "";
+   
+    // if (!localStorage.getItem(searchCity)) {
+    //     localStorage.setItem(searchCity, JSON.stringify(searchCity));
+    // }
+
+    fetch(openWeatherUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (weatherData) {
+            var cityName = weatherData.name;
+            var currentTemp = weatherData.main.temp;
+            var currentHumidity = weatherData.main.humidity;
+            var currentWindDir = weatherData.wind.deg;
+            var currentWind = weatherData.wind.speed
+            var currentWeatherSummary = weatherData.weather.description;
+
+            // var selectedWeatherData = [cityName, currentTemp, currentWeatherSummary];
+            //renders the searched city name and weather data
+            renderTodayForecast(cityName, currentTemp, currentHumidity, currentWindDir, currentWind, currentWeatherSummary);
+            //clears the input form
+
+            globalCityButton.textContent = cityName;
+            
+            // getLocalStorage();//calls getLocalStorage
+            localStorage.setItem("lastSearchedCity", cityName);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+};
+
+function renderTodayForecast(cityName, currentTemp, currentHumidity, currentWindSpeed, currentWindDir, currentWeatherSummary) {
+    currentWeather.innerHTML = "";
+
+    // var currentWeatherDiv = document.createElement("div");
+    // currentWeatherDiv.classList.add('current-weather-div')
+
+    var todayWeatherCity = document.createElement('h4');
+    todayWeatherCity.textContent = cityName;
+   
+    var currentWeatherTemp = document.createElement('p');
+    currentWeatherTemp.textContent = `Temperature: ${currentTemp} deg. F`;
+
+    var currentHumidity = document.createElement('p');
+    currentHumidity.textContent = `Humidity: ${currentHumidity} %`;
+
+    var currentWindSpeed = document.createElement('p');
+    currentWindSpeed.textContent = `Wind Speed: ${currentWindSpeed} mph`;
+
+    var currentWindDir = document.createElement('p');
+    currentWindDir.textContent = `Wind Dir: ${currentWindDir} deg. F`;
+    
+    var currentWeatherSummary = document.createElement('p');
+    currentWeatherSummary.textContent = `Summary: ${currentWeatherSummary}`;
+
+    currentWeather.append(todayWeatherCity);
+    currentWeather.append(currentWeatherTemp);
+    currentWeather.append(currentWeatherSummary);
+
+    
+    return;
+}
 //Maybe adjust this later to make unique key integer and use the corresponding value as the button text?
 let getLocalStorage = () => {
     Object.keys(localStorage).forEach((key) => {
@@ -23,7 +98,7 @@ let getLocalStorage = () => {
 
             document.getElementById("current-weather").innerHTML = "";
 
-            renderTodayForecast(cityName, currentTemp, currentWeatherSummary);
+            renderTodayForecast(cityName, currentTemp, currentHumidity, currentWindSpeed, currentWindDir, currentWeatherSummary);
         });
         var localStorageCity = JSON.parse(localStorage.getItem(key));
         cityWeatherButton.textContent = localStorageCity;
@@ -33,63 +108,7 @@ let getLocalStorage = () => {
 
 getLocalStorage();
 
-//Call to API passing the searched city name and concat it into the API along with the key and conversion to imperial system format.
-function getWeatherByCity(event) {
-    event.preventDefault();
-    var searchCity = document.getElementById("searched-city-input").value;
-
-    var openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + openWeatherApiKey + "&units=imperial";
-
-    document.getElementById("current-weather").innerHTML = "";
-   
-    if (!localStorage.getItem(searchCity)) {
-        localStorage.setItem(searchCity, JSON.stringify(searchCity));
-    }
-
-    fetch(openWeatherUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (weatherData) {
-            var cityName = weatherData.name;
-            var currentTemp = weatherData.main.temp;
-            var currentWeatherSummary = weatherData.weather[0].description;
-            var selectedWeatherData = [cityName, currentTemp, currentWeatherSummary];
-            //renders the searched city name and weather data
-            renderTodayForecast(cityName, currentTemp, currentWeatherSummary);
-            //clears the input form
-          
-            if(globalCityButton) {
-                globalCityButton.textContent = cityName;
-            }
-            // getLocalStorage();//calls getLocalStorage
-
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-};
-
-function renderTodayForecast(cityName, currentTemp, currentWeatherSummary) {
-        var todayWeatherCity = document.createElement('h4');
-        todayWeatherCity.textContent = cityName;
-
-        var currentWeatherData = document.createElement('ul');
-        var currentWeatherLineItem1 = document.createElement('li');
-        currentWeatherLineItem1.textContent = `Temperature: ${currentTemp} deg. F`;
-        var currentWeatherLineItem2 = document.createElement('li');
-        currentWeatherLineItem2.textContent = `Summary: ${currentWeatherSummary}`;
-
-        currentWeather.append(todayWeatherCity);
-        currentWeather.append(currentWeatherLineItem1, currentWeatherLineItem2);
-        currentWeather.append(currentWeatherData);
-        
-        return;
-    }
-//     );
-// };
-
-
+renderTodayForecast();
 
 
 function clearHistory(event) {
@@ -100,10 +119,7 @@ function clearHistory(event) {
 
 
 //Event listener for city search Button that also refreshes browser
-searchWeatherButton.addEventListener('click', function(event) {
-    getWeatherByCity(event)
-    location.reload();
-});
+searchWeatherButton.addEventListener('click', getWeatherByCity);
 
 clearCityHistoryButton.addEventListener('click', function(event) {
     clearHistory(event)
