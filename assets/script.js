e7b387057f9f0ceeca6503fb19f3470d1eba460a
Alt B 2022 <massie.w@gmail.com>
2023-03-28 01:04:03 -0700

    Had to bail on the modular approach of compass.js.  Dropped fucntion directly into script.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 //Some notes and psuedo-code about what I'm trying to do to finish this app:
 //Redirected the flow of data slightly to isolate the search button functionality to establish city name rather than have it be var in the api fetch function.  Will pass cityName as data to API function and have it be pushed to local storage array.  Will use separately the lastSearchedCity to persist the page data.  
 
-import { getCardinalDirection } from "./compass";
+// import { getCardinalDirection } from "./compass";// imports getCardinalDirection from compass.js file
 var openWeatherApiKey = "eaf99af1f4ee974c35e4e4ec7368e660";
 var searchWeatherButton = document.getElementById("search-by-city-button");
 var clearCityHistoryButton = document.getElementById("clear-history-btn");
@@ -11,6 +11,34 @@ var currentWeather = document.getElementById("current-weather");
 var todayWeatherIcon = document.getElementById("current-icon");
 var fiveDayParentEl = document.getElementById("five-day-forecast");
 
+function getCardinalDirection(degrees) {
+    const degreeRanges = [ 
+      { direction: "N", range: [0, 11.25] },
+      { direction: "NNE", range: [11.25, 33.75] },
+      { direction: "NE", range: [33.75, 56.25] },
+      { direction: "ENE", range: [56.25, 78.75] },
+      { direction: "E", range: [78.75, 101.25] },
+      { direction: "ESE", range: [101.25, 123.75] },
+      { direction: "SE", range: [123.75, 146.25] },
+      { direction: "SSE", range: [146.25, 168.75] },
+      { direction: "S", range: [168.75, 191.25] },
+      { direction: "SSW", range: [191.25, 213.75] },
+      { direction: "SW", range: [213.75, 236.25] },
+      { direction: "WSW", range: [236.25, 258.75] },
+      { direction: "W", range: [258.75, 281.25] },
+      { direction: "WNW", range: [281.25, 303.75] },
+      { direction: "NW", range: [303.75, 326.25] },
+      { direction: "NNW", range: [326.25, 348.75] },
+      { direction: "N", range: [348.75, 360] },
+    ];
+  
+    for (let i = 0; i < degreeRanges.length; i++) {
+      if (degrees >= degreeRanges[i].range[0] && degrees < degreeRanges[i].range[1]) {
+        return degreeRanges[i].direction;
+      }
+    }
+    return "N"; // default direction if degrees is not within any of the defined ranges
+  }
 
 
 // console.log(compass)
